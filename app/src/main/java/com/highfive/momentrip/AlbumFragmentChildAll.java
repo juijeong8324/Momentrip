6378019cda1d2653382f43405d8ecbca6993f9c9
juijeong8324 <juijeong8324@gmail.com>
2021-11-28 22:58:27 +0900

    의정

---
+++ app/src/main/java/com/highfive/momentrip/AlbumFragmentChildAll.java
@@ -0,0 +1,32 @@
+package com.highfive.momentrip;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AlbumFragmentChildAll extends Fragment {
+    //이 프래그먼트는 My moment 전체 폴라로이드 부분입니다!
+    public static AlbumFragmentChildAll newInstance(){
+           return new AlbumFragmentChildAll();
+    }
+
+   /* @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }*/
+
+    @Nullable
+    @Override
+    // 프래그먼트도 액티비티를 본떠서 만들었기 때문에 수명주기 메서드를 가질 수 있게한다
+    // 이 메소드 안에서는 인플레이션 가능!
+    public View onCreateView(LayoutInflater inflater, @Nullable  ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_album_all, container,false );
+        // 인플레이션 과정을 통해 ViewGroup이라는 객체가 전달
+
+        return rootView; // 프래그먼트 안에 들어가는 최상위 뷰
+    }
+}
